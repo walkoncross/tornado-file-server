fa67dd8b4bce3cbca1d5be4d8e07ffdb4e11cd97
Yafei Zhao <zhaoyafei0210@gmail.com>
2022-08-08 18:54:01 +0800

    update run_tornado_file_server.sh

--- run_tornado_file_server.sh
+++ run_tornado_file_server.sh
@@ -1,14 +1,33 @@
 #!/bin/bash
 
 # python3 --version
+root_dir=./
+
+# echo "number of args: $#"
+
+if [[ $# -gt 0 ]]; then
+    root_dir=$1
+fi
 
 port=8899
-root_dir=./
 max_items=50
 
 ip=`python -m tornado_file_server.get_ip`
 
-echo "===> Open your browser and input: $ip:$port or localhost:$port"
-# nohup python3 tornado_file_server.py --log-file-prefix=./log --port=$port --max-items=$max_items $root_dir &
+echo "root_dir: $root_dir"
+echo "IP: $ip"
+echo "Port: $port"
 
-nohup python -m tornado_file_server.serving --port $port --max-items $max_items $root_dir &
+echo "===> Open your browser and input: $ip:$port or localhost:$port"
+# nohup python3 tornado_file_server.py 
+#     --log-file-prefix=./log 
+#     --port=$port \
+#     --max-items=$max_items \
+#     $root_dir \
+#     &
+
+nohup python -m tornado_file_server.serving \
+    --port $port \
+    --max-items $max_items \
+    $root_dir \
+    &
